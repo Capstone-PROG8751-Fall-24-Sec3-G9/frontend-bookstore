d96885366f3ba09e35b897f936330b0ee3bb80c5
Rushil-Y <rushilreddy3@gmail.com>
2024-11-21 16:45:53 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BookStorePage from "./components/BookStore";
+
+function App() {
+  return (
+    <div className="App">
+      <BookStorePage />
+    </div>
+  );
+}
+
+export default App;
